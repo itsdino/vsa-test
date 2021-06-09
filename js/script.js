13c3e517c794709c53820bd7a33853aa6f2511d4
itsdino <62812052+itsdino@users.noreply.github.com>
2021-06-09 21:53:38 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -103,21 +103,6 @@ const heroNavbar = () => {
 
 //
 
-
-// gallery calls
-for (let i=1; i <= 14; i++) {
-  lightGallery(document.getElementById('gallery-' + i), {
-    startClass: 'lg-start-fade',
-    mode: 'lg-fade',
-    preload: 2,
-    enableDrag: false,
-    enableSwipe: false,
-    backdropDuration: 300,
-    mousewheel: false,
-    download: false
-  });
-}
-
 // jQuery smooth scroll to element
 $("#navbar ul li a, .site-link").on("click", function (e) {
   if (this.hash !== "") {
